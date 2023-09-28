1bb67cf9bfdb49a4dac1f551f345e2177d7ccf56
manish7479dlp <manish7479dlp@gmail.com>
2023-09-29 01:55:43 +0530

    ui: create template

---
+++ screens/Expances.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Expances = () => {
+    return (
+        <View>
+            <Text>Expances</Text>
+        </View>
+    )
+}
+
+export default Expances
+
+const styles = StyleSheet.create({})
---
+++ screens/Routine.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Routine = () => {
+    return (
+        <View>
+            <Text>Routine</Text>
+        </View>
+    )
+}
+
+export default Routine
+
+const styles = StyleSheet.create({})
