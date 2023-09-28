9c0e8df3ea6af97bc43bcf1332ababc1dbf27767
manish7479dlp <manish7479dlp@gmail.com>
2023-09-29 01:56:06 +0530

    merge: userNavigation in app

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/Login';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import UserHome from './screens/UserHome';
+import MembersList from './components/MembersList';
+import UserNavigation from './navigation/userNavigation';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Login />
+
+      <UserNavigation />
 
     </SafeAreaProvider>
   )
