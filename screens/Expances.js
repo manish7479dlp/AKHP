a5c51e66baeb20cb9894b1f5dcbf17d1474e005f
manish7479dlp <manish7479dlp@gmail.com>
2023-09-29 17:37:26 +0530

    ui: implement Expances screen feature

--- screens/Expances.js
+++ screens/Expances.js
@@ -1,14 +1,45 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import UserData from '../components/UserData'
+import AccordionContainer from '../components/AccordionContainer'
+import { FontAwesome } from '@expo/vector-icons';
+
+import color from '../constant/color'
+
 
 const Expances = () => {
+
     return (
-        <View>
-            <Text>Expances</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <UserData />
+            <TotalExpanses />
+            <AccordionContainer />
+        </SafeAreaView>
     )
 }
 
 export default Expances
 
-const styles = StyleSheet.create({})
+const TotalExpanses = () => {
+    return (
+        <View style={styles.expansesTotalContainer}>
+            <Text style={{ color: "white", fontWeight: "500", fontSize: 17 }}>Monthly Expanses</Text>
+            <Text style={{ color: "white", fontWeight: "600", fontSize: 18 }}> <FontAwesome name="rupee" size={17} color="white" /> 1456</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: "100%"
+    },
+    expansesTotalContainer: {
+        backgroundColor: color.second,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+    }
+})
