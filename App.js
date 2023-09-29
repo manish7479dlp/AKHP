bba789c9cd277bce0903fae579b96bdaeaa6824c
manish7479dlp <manish7479dlp@gmail.com>
2023-09-29 20:26:55 +0530

    ui: add stack navigation functionality

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/Login';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import UserHome from './screens/UserHome';
-import MembersList from './components/MembersList';
 import UserNavigation from './navigation/userNavigation';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import AdminNavigation from './navigation/AdminNavigation';
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <SafeAreaProvider>
+      <NavigationContainer>
 
-      <UserNavigation />
+        <Stack.Navigator>
+          <Stack.Screen options={{
+            headerShown: false
+          }} name='login' component={Login} />
+
+          <Stack.Screen options={{
+            headerShown: false,
+            animation: 'slide_from_right'
+          }} name='user' component={UserNavigation} />
+
+          <Stack.Screen options={{
+            headerShown: false,
+            animation: 'slide_from_right'
+          }} name='admin' component={AdminNavigation} />
+        </Stack.Navigator>
+
+      </NavigationContainer>
+
 
     </SafeAreaProvider>
   )
