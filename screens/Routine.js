cbd3a215789b9446f6216bea0f1d69c335ecebfd
manish7479dlp <manish7479dlp@gmail.com>
2023-09-29 12:10:56 +0530

    ui: create Routine

--- screens/Routine.js
+++ screens/Routine.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import MealDetails from '../components/MealDetails'
+import UserData from '../components/UserData'
+
+var { width, height } = Dimensions.get('window');
 
 const Routine = () => {
     return (
-        <View>
-            <Text>Routine</Text>
-        </View>
+        <SafeAreaView>
+            <UserData />
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                style={styles.container}
+            >
+                <MealDetails title={"Monday "} lunchMeal={"Chawal Dal Egg Curry"} dinnerMeal={"Chawal Dal Aalu Sabji"} />
+                <MealDetails title={"Tuesday"} lunchMeal={"Chawal Dal Egg Curry"} dinnerMeal={"Chawal Dal Aalu Sabji"} />
+                <MealDetails title={"Today Meal"} lunchMeal={"Chawal Dal Egg Curry"} dinnerMeal={"Chawal Dal Aalu Sabji"} />
+
+                <MealDetails title={"Today Meal"} lunchMeal={"Chawal Dal Egg Curry"} dinnerMeal={"Chawal Dal Aalu Sabji"} />
+                <MealDetails title={"Today Meal"} lunchMeal={"Chawal Dal Egg Curry"} dinnerMeal={"Chawal Dal Aalu Sabji"} />
+                <MealDetails title={"Today Meal"} lunchMeal={"Chawal Dal Egg Curry"} dinnerMeal={"Chawal Dal Aalu Sabji"} />
+                <MealDetails title={"Today Meal"} lunchMeal={"Chawal Dal Egg Curry"} dinnerMeal={"Chawal Dal Aalu Sabji"} />
+
+            </ScrollView>
+
+        </SafeAreaView>
     )
 }
 
 export default Routine
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: height * .07
+    }
+})
