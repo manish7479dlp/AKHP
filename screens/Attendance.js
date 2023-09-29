21e7e61537e94731f4862494b86da97be51d5749
manish7479dlp <manish7479dlp@gmail.com>
2023-09-29 19:52:59 +0530

    ui: some changes is done in Attendance component

--- screens/Attendance.js
+++ screens/Attendance.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import color from "../constant/color"
+
+var { width, height } = Dimensions.get('window');
+
 
 const Attendance = () => {
     return (
-        <View>
-            <Text>Attendance</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={{ fontSize: 18, fontWeight: "700", color: color.second }}>It is not implemented yet.</Text>
+        </SafeAreaView>
     )
 }
 
 export default Attendance
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: color.background,
+        height: height,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    }
+})
