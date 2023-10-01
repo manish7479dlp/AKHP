66a1b93d61928176c512fdf5b54f0ae9251ba279
manish7479dlp <manish7479dlp@gmail.com>
2023-10-02 00:10:11 +0530

    add signIn and editUser functionality

--- Helper/api.js
+++ Helper/api.js
@@ -21,6 +21,60 @@ const userLogin = async (mobile) => {
     }
 }
 
+const signIn = async (name, year, advance, mobile) => {
+    const URL = BASE_URL + "/api/auth/signin"
+    const { token } = useSelector((state) => state.user.data)
+
+    try {
+        const response = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                name: name,
+                year,
+                advance,
+                mobile
+            })
+
+        });
+        const user = await response.json();
+        console.log(user)
+        return user;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const editUser = async (name, year, advance, mobile) => {
+    const URL = BASE_URL + "/api/auth/signin"
+    const { token } = useSelector((state) => state.user.data)
+
+    try {
+        const response = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                year,
+                advance,
+                mobile,
+                name: name
+            })
+
+        });
+        const user = await response.json();
+        console.log(user)
+        return user;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 const getRoutine = async () => {
     const URL = BASE_URL + "/api/auth/login"
     const { token } = useSelector((state) => state.user.data)
