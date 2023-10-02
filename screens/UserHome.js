081ea456f2b73c509eea2ddb4c35755095706187
manish7479dlp <manish7479dlp@gmail.com>
2023-10-02 19:05:14 +0530

    Today meal make dynamic data fetch

--- screens/UserHome.js
+++ screens/UserHome.js
@@ -1,21 +1,37 @@
 import { Dimensions, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import color from "../constant/color"
 import { MaterialCommunityIcons, Ionicons, Feather } from '@expo/vector-icons';
 import MembersList from '../components/MembersList';
 import MealDetails from '../components/MealDetails';
 import UserData from '../components/UserData';
+import { getRoutineDayWise } from '../Helper/api';
 
 var { width, height } = Dimensions.get('window');
 
 
 const UserHome = () => {
+    const [meal, setMeal] = useState()
+
+    useEffect(() => {
+        getRoutine()
+    }, [])
+
+    const getRoutine = async () => {
+        try {
+            const response = await getRoutineDayWise()
+            setMeal(response.routine)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <SafeAreaView>
             <View style={styles.container}>
                 <UserData />
-                <MealDetails title={"Today Meal"} lunchMeal={"Chawal Dal Egg Curry"} dinnerMeal={"Chawal Dal Aalu Sabji"} edit={false} />
+                <MealDetails title={"Today Meal"} lunchMeal={meal?.lunch} dinnerMeal={meal?.dinner} edit={false} />
                 <MembersList mb={height * .5} />
 
 
