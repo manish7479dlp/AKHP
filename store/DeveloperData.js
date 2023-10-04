0d6c6322ca8c2b6aa46a2eb6a034845dade9d213
manish7479dlp <manish7479dlp@gmail.com>
2023-10-04 19:42:41 +0530

    add: developer img path

--- store/DeveloperData.js
+++ store/DeveloperData.js
@@ -2,6 +2,7 @@ export default DEVLOPER_DATA = {
     first: {
         name: "Manish Kumar",
         role: "MERN Stack || React-Native Developer",
+        img: require("../assets/devloperImg/manish.jpg"),
         instagramLink: "https://www.instagram.com/manish7479dlp/",
         linkedInLink: "https://www.linkedin.com/in/manish7479dlp/",
         githubLink: "https://github.com/manish7479dlp",
@@ -9,6 +10,7 @@ export default DEVLOPER_DATA = {
     second: {
         name: "Injamul MD Mollah",
         role: "Backend Developer",
+        img: require("../assets/devloperImg/injamul.png"),
         instagramLink: "https://www.instagram.com/manish7479dlp/",
         linkedInLink: "https://www.linkedin.com/in/manish7479dlp/",
         githubLink: "https://github.com/manish7479dlp",
@@ -16,11 +18,10 @@ export default DEVLOPER_DATA = {
     third: {
         name: "MD Owais Raza",
         role: "System Architect",
-        instagramLink: "https://www.instagram.com/manish7479dlp/",
-        linkedInLink: "https://www.linkedin.com/in/manish7479dlp/",
-        githubLink: "https://github.com/manish7479dlp",
-        // instagramLink: "",
-        // linkedInLink: "",
-        // githubLink: "",
+        img: require("../assets/devloperImg/owais.jpg"),
+        instagramLink: "https://www.instagram.com",
+        linkedInLink: "https://www.linkedin.com/in/md-owais-raza",
+        githubLink: "https://github.com/0xOwais",
+
     }
 }
