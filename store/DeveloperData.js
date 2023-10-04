c8e9ad3c3794de9c12737bc094d0e4ddde9eb12e
manish7479dlp <manish7479dlp@gmail.com>
2023-10-04 19:12:19 +0530

    ui: featch developer data dynamically

---
+++ store/DeveloperData.js
@@ -0,0 +1,26 @@
+export default DEVLOPER_DATA = {
+    first: {
+        name: "Manish Kumar",
+        role: "MERN Stack || React-Native Developer",
+        instagramLink: "https://www.instagram.com/manish7479dlp/",
+        linkedInLink: "https://www.linkedin.com/in/manish7479dlp/",
+        githubLink: "https://github.com/manish7479dlp",
+    },
+    second: {
+        name: "Injamul MD Mollah",
+        role: "Backend Developer",
+        instagramLink: "https://www.instagram.com/manish7479dlp/",
+        linkedInLink: "https://www.linkedin.com/in/manish7479dlp/",
+        githubLink: "https://github.com/manish7479dlp",
+    },
+    third: {
+        name: "MD Owais Raza",
+        role: "System Architect",
+        instagramLink: "https://www.instagram.com/manish7479dlp/",
+        linkedInLink: "https://www.linkedin.com/in/manish7479dlp/",
+        githubLink: "https://github.com/manish7479dlp",
+        // instagramLink: "",
+        // linkedInLink: "",
+        // githubLink: "",
+    }
+}
