9dc83f6177d1763f45f0ad602d362cb33838a05c
manish7479dlp <manish7479dlp@gmail.com>
2023-10-06 02:09:53 +0530

    ui: create attendance component and implements its featue

--- screens/Attendance.js
+++ screens/Attendance.js
@@ -1,29 +1,116 @@
-import { Dimensions, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Ionicons, Feather } from '@expo/vector-icons';
+
 
 import color from "../constant/color"
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 var { width, height } = Dimensions.get('window');
 
 
 const Attendance = () => {
     return (
-        <SafeAreaView style={styles.container}>
-            <Text style={{ fontSize: 18, fontWeight: "700", color: color.second }}>It is not implemented yet.</Text>
+        <SafeAreaView >
+            <ScrollView>
+                <Scan />
+
+                <View style={styles.container}>
+                    <Text style={styles.title}>Today's Attendance</Text>
+                    <View style={styles.attendanceContainer}>
+                        <View style={styles.lunchContainer}>
+                            <Text style={{ color: "white", fontWeight: "500" }}>
+                                <Feather name="sun" size={20} color="white" />  Lunch :
+                            </Text>
+                        </View>
+
+                        <View style={styles.lunchContainer}>
+                            <Text style={{ color: "white", fontWeight: "500" }}>
+                                <Ionicons name="cloudy-night-outline" size={20} color={"white"} />  Dinner :
+                            </Text>
+                        </View>
+
+                    </View>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
 
+const Scan = () => {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(true);
+
+    useEffect(() => {
+        const getBarCodeScannerPermissions = async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        };
+
+        getBarCodeScannerPermissions();
+
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        //send request from their
+        console.log(type, data)
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    return (
+        <View style={styles.scannerContainer}>
+            <BarCodeScanner
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+            />
+            {scanned && <Button title={'Tap for Attendance'} onPress={() => setScanned(false)} />}
+        </View>
+    );
+}
+
+
+
 export default Attendance
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: color.background,
-        height: height,
+    scannerContainer: {
+        height: height * .6,
+        width: width,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
 
+    },
+    container: {
+        marginTop: 10,
+        padding: 20,
+        backgroundColor: color.background,
+        display: 'flex',
+        justifyContent: 'center'
+
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: '700',
+        color: color.second
+    },
+    lunchContainer: {
+        margintop: 10,
+        padding: 5,
+    },
+    attendanceContainer: {
+        backgroundColor: color.second,
+        borderRadius: 20,
+        padding: 10,
+        margin: 10
     }
+
 })
