f1b945d2c3600c95aabbaaa45712283e92b60c99
manish7479dlp <manish7479dlp@gmail.com>
2023-10-08 00:24:10 +0530

    remove: marginTop from button

--- screens/Login.js
+++ screens/Login.js
@@ -201,7 +201,7 @@ const styles = StyleSheet.create({
     button: {
         width: width,
         alignItems: 'center',
-        marginTop: 5
+        // marginTop: 5
     },
     buttonLabel: {
         color: 'white',
