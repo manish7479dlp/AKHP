8edd594d6c6b375991b174aeca532f9d6466c954
manish7479dlp <manish7479dlp@gmail.com>
2023-10-09 23:21:21 +0530

    add: advanceMoney feature

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,14 +1,161 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Switch, Text, View } from 'react-native'
 import React from 'react'
+import Skelton from "../components/Skelton"
+
+import { useState } from 'react'
+import InputField from '../components/InputField'
+import CustomButton from '../components/CustomButton'
+import color from '../constant/color'
+
+import { SelectList } from 'react-native-dropdown-select-list'
+
+
+const { width, height } = Dimensions.get("screen")
+
+const DATA = ["Spend", "Advance"]
 
 const Transaction = () => {
+    const [spend, setSpend] = useState(false)
     return (
-        <View>
-            <Text>Transaction</Text>
+        <Skelton>
+            <View style={styles.container}>
+
+                {/* //spend or advance money show button */}
+                <CustomSwitch label={"Spend"} value={spend} setValue={setSpend} />
+
+                {/* render (spend and Advance Money component) on the basis of upper switch */}
+                {spend ? <Spend /> : <AdvanceMoney />}
+
+
+            </View>
+        </Skelton>
+    )
+}
+const CustomSwitch = ({ value, setValue, label }) => {
+    return (
+        <View style={{ width: width * .9, paddingLeft: 8 }}>
+            <View style={styles.admin} >
+                <Text style={[styles.label, styles.adminLabel]}>
+                    {label}
+                </Text>
+                <Switch
+                    value={value}
+                    onValueChange={(value) => setValue(value)}
+                    trackColor={{ false: color.third, true: color.layer }}
+                    thumbColor={value ? color.second : '#f4f3f4'}
+
+                />
+
+            </View>
+        </View>
+    )
+}
+
+const AdvanceMoney = () => {
+    const [mobile, setMobile] = useState()
+    const [advance, setAdvance] = useState()
+    const [description, setDescription] = useState()
+    const [loading, setLoading] = useState()
+
+    const handleAdvance = () => {
+
+    }
+
+    return (
+        <View style={styles.advanceContainer}>
+
+
+            <Text style={styles.advanceTitle}>Advance Money</Text>
+            <InputField label={"Mobile Number"} keyBoardType='numeric' value={mobile} onValueChange={setMobile} />
+            <InputField label={"Advance Money"} keyBoardType='numeric' value={advance} onValueChange={setAdvance} />
+            <InputField label={"Description"} value={description} onValueChange={setDescription} />
+
+            <CustomButton btnLabel={"Add Advance"} btnClick={handleAdvance} loading={loading} />
+
+        </View>
+    )
+}
+
+const Spend = () => {
+    const [isCash, setIsCash] = useState(false)
+    const [itemName, setItemName] = useState()
+    const [quantity, setQuantity] = useState()
+    const [amount, setAmount] = useState()
+    const [loading, setLoading] = useState()
+    const [selected, setSelected] = React.useState("");
+    const data = [
+        { key: '1', value: 'Mobiles', disabled: true },
+        { key: '2', value: 'Appliances' },
+        { key: '3', value: 'Cameras' },
+        { key: '4', value: 'Computers', disabled: true },
+        { key: '5', value: 'Vegetables' },
+        { key: '6', value: 'Diary Products' },
+        { key: '7', value: 'Drinks' },
+    ]
+
+
+    return (
+        <View style={styles.spendContainer}>
+            <Text style={styles.advanceTitle}>Spend Money</Text>
+
+            <SelectList
+                setSelected={(val) => setSelected(val)}
+                data={data}
+                save="value"
+            />
+            <InputField label={"Item Name"} value={itemName} setChangeValue={setItemName} />
+            <InputField label={"Quantity"} value={quantity} setChangeValue={setQuantity} />
+            <InputField label={"Amount"} keyBoardType='numeric' value={amount} setChangeValue={setAmount} />
+
+            <CustomSwitch label={"Cash"} value={isCash} setValue={setIsCash} />
+            <CustomButton loading={loading} btnLabel={"Save"} />
         </View>
     )
 }
 
 export default Transaction
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    admin: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    adminLabel: {
+        marginRight: 5,
+        fontWeight: '500',
+        fontSize: 23
+    },
+    container: {
+        width: width,
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    advanceContainer: {
+        width: width,
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    advanceTitle: {
+        fontWeight: '700',
+        fontSize: 35,
+        color: color.second,
+        marginBottom: 5
+    },
+
+    // spent style\
+
+    spendContainer: {
+        width: width,
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }
+
+})
